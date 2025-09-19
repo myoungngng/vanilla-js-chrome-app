6c845f6d0f802516eb2d2cde9fc414d1cb9fee95
myoungngng <msy411@naver.com>
2025-09-19 14:42:09 +0900

    Today's Study Log

---
+++ app.js
@@ -0,0 +1,30 @@
+/* 
+// const, let, var
+const a = 10; //const는 재할당 X, 자주 사용됨
+const b = 2;
+var c = 15; //var은 재할당 O, var의 단점을 보완하고자 const와 let을 사용
+let myName = "Cindy"; //let은 재할당 O, 필요할 때만 사용됨
+
+console.log(a + b); //12
+console.log(a * b); //20
+console.log(a / b); //5
+console.log("현재 c의 값은 " + c); //15
+console.log("안녕, " + myName); //Cindy
+
+c = 20
+myName = "cindY";
+console.log("업데이트된 c의 값은 " + c); //20
+console.log("너의 이름은 " + myName + "?"); //cindY
+*/
+
+/*--------------------------------------------------*/
+
+/*
+// Booleans 
+const amIFat = true;
+const abc = null;
+let something;
+console.log(amIFat); //true, 문자열 "true"와는 다름
+console.log(null); //null, 값이  없음을 지정함
+console.log(something); //undefined, 값이 정의되지 않음 
+*/
